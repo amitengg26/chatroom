9e616bd31e8f6ea90d21e141c2904fa58147a4e3
amitengg26 <54315532+amitengg26@users.noreply.github.com>
2020-05-19 18:14:26 +0530

    ngrok added

--- chatclient.js
+++ chatclient.js
@@ -105,7 +105,7 @@ function connect() {
   if (document.location.protocol === "https:") {
     scheme += "s";
   }
-  serverUrl = scheme + "://" + myHostname + ":6503";
+  serverUrl = 'https://1de78e25.ngrok.io/';//scheme + "://" + myHostname + ":6503";
 
   log(`Connecting to server: ${serverUrl}`);
   connection = new WebSocket(serverUrl, "json");
